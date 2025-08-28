66130e76b60ea29daa51a5d31ac304e22ba3bf0c
montaj25 <muhammadmontaj@gmail.com>
2025-08-29 04:25:25 +0600

    added the js section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log('js file is connected')
+// love life btn
+const lifeButtons = document.getElementsByClassName("life")
+for (const lifeButton of lifeButtons) {
+    lifeButton.addEventListener("click", function(){ 
+        const loveNumber = parseInt(document.getElementById("love-number").innerText)
+        const newLoveNumber = loveNumber + 1;
+        document.getElementById("love-number").innerText = newLoveNumber;
+    })
+}
+
+// coin btn
+const coinBtns = document.getElementsByClassName("coin-btn ")
+for (const coinBtn of coinBtns) {
+    coinBtn.addEventListener("click", function(){
+        const coinNumber = parseInt(document.getElementById("coin-number").innerText)
+        const newCoinNumber = coinNumber - 20;
+        document.getElementById("coin-number").innerText = newCoinNumber;
+    })
+} 
