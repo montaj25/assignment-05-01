c2feb0ec3734a373ef1c467373711bc8c8551773
montaj25 <muhammadmontaj@gmail.com>
2025-08-29 16:10:17 +0600

    added this section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 console.log('js file is connected')
+
+const callHistoryData = [];
 // love life btn
 const lifeButtons = document.getElementsByClassName("life")
 for (const lifeButton of lifeButtons) {
@@ -8,6 +10,8 @@ for (const lifeButton of lifeButtons) {
         document.getElementById("love-number").innerText = newLoveNumber;
     })
 }
+
+
 
 // coin btn
 const coinBtns = document.getElementsByClassName("coin-btn")
@@ -20,20 +24,53 @@ for (const coinBtn of coinBtns) {
 
             const subTitle = coinBtn.parentNode.parentNode.parentNode.childNodes[3].innerText
             const subTitleNumber = coinBtn.parentNode.parentNode.parentNode.childNodes[5].innerText
-            alert("📞"+" "+subTitle+" "+ subTitleNumber+"..........");
+            alert("📞" + " " + subTitle + " " + subTitleNumber + "..........");
+
+            const data = {
+                name: subTitle,
+                date: new Date().toLocaleTimeString()
+            }
+            callHistoryData.push(data)
+            console.log(callHistoryData)
         }
-        else{
+        else {
             alert("❌ You don't have sufficient coin. It will take at least twenty coins to make a call.")
         }
+
+
     })
-} 
+
+}
 // copy number
 
 const copyBtns = document.getElementsByClassName("copy-btn")
 for (const copyBtn of copyBtns) {
-    copyBtn.addEventListener("click", function(){ 
+    copyBtn.addEventListener("click", function () {
         const copyNumber = parseInt(document.getElementById("copy-number").innerText)
         const newCopyNumber = copyNumber + 1;
         document.getElementById("copy-number").innerText = newCopyNumber;
     })
+}
+
+
+
+// call history
+const callHistoryContainer = document.getElementById("call-history-container")
+// callHistoryContainer.innerText = " "
+for (const data of callHistoryData) {
+    const div = document.createElement("div")
+    div.innerHTML = `
+                    <div class="flex justify-between items-center bg-[#FAFAFA] p-4 m-8 rounded-xl">
+                        <div>
+                            <h2 class="font-semibold text-xl">${data.name}</h2>
+                            <p class="text-gray-500 text-xl">999</p>
+                        </div>
+                        <div>
+                            <p>${data.date}/p>
+                        </div>
+                    </div>
+
+    `
+    const all = callHistoryContainer.appendChild(div)
+    console.log(all)
 }
