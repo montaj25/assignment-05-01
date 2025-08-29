6410910677653b068d7277dda4c016510884228d
montaj25 <muhammadmontaj@gmail.com>
2025-08-29 10:54:46 +0600

    added this JS part

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log('js file is connected')
 // love life btn
 const lifeButtons = document.getElementsByClassName("life")
 for (const lifeButton of lifeButtons) {
-    lifeButton.addEventListener("click", function(){ 
+    lifeButton.addEventListener("click", function () {
         const loveNumber = parseInt(document.getElementById("love-number").innerText)
         const newLoveNumber = loveNumber + 1;
         document.getElementById("love-number").innerText = newLoveNumber;
@@ -10,11 +10,30 @@ for (const lifeButton of lifeButtons) {
 }
 
 // coin btn
-const coinBtns = document.getElementsByClassName("coin-btn ")
+const coinBtns = document.getElementsByClassName("coin-btn")
 for (const coinBtn of coinBtns) {
-    coinBtn.addEventListener("click", function(){
+    coinBtn.addEventListener("click", function () {
         const coinNumber = parseInt(document.getElementById("coin-number").innerText)
-        const newCoinNumber = coinNumber - 20;
-        document.getElementById("coin-number").innerText = newCoinNumber;
+        if (coinNumber >= 20) {
+            const newCoinNumber = coinNumber - 20;
+            document.getElementById("coin-number").innerText = newCoinNumber;
+
+            const subTitle = coinBtn.parentNode.parentNode.parentNode.childNodes[3].innerText
+            const subTitleNumber = coinBtn.parentNode.parentNode.parentNode.childNodes[5].innerText
+            alert("📞"+" "+subTitle+" "+ subTitleNumber+"..........");
+        }
+        else{
+            alert("❌ You don't have sufficient coin. It will take at least twenty coins to make a call.")
+        }
     })
 } 
+// copy number
+
+const copyBtns = document.getElementsByClassName("copy-btn")
+for (const copyBtn of copyBtns) {
+    copyBtn.addEventListener("click", function(){ 
+        const copyNumber = parseInt(document.getElementById("copy-number").innerText)
+        const newCopyNumber = copyNumber + 1;
+        document.getElementById("copy-number").innerText = newCopyNumber;
+    })
+}
